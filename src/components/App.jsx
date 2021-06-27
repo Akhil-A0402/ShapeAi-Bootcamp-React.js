a5bf567717635b31cbe719d5cfe488e0200ad56e
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-06-27 10:13:37 +0530

    Bootcamp

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Courses from "./Courses";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+      <Courses />
+    </div>
+  );
+}
---
+++ src/components/Courses.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js 7-days Bootcamp</h1>
+      <p>Lorem ipsum...</p>
+    </div>
+  );
+}
+
+export default Courses;
